a55147d448a69d877b422337d4e599e8e4ccf18f
Vitalii Rudkin <vitasik.rudkin@gmail.com>
2021-08-23 09:54:10 +0300

    Add pop-up modal for rules

--- app.js
+++ app.js
@@ -290,6 +290,7 @@ document.addEventListener("DOMContentLoaded", () => {
         squares[index].classList.remove("taken");
         squares[index].classList.remove("tetromino");
         squares[index].style.backgroundColor = "";
+        squares[index].style.backgroundImage = "none";
       });
       squares.forEach((cell) => grid.appendChild(cell));
     }
@@ -343,6 +344,7 @@ document.addEventListener("DOMContentLoaded", () => {
           squares[index].classList.remove("taken");
           squares[index].classList.remove("tetromino");
           squares[index].style.backgroundColor = "";
+          squares[index].style.backgroundImage = "none";
         });
         const squaresRemoved = squares.splice(i, width);
         squares = squaresRemoved.concat(squares);
@@ -365,4 +367,30 @@ document.addEventListener("DOMContentLoaded", () => {
       startBtn.disabled = true;
     }
   }
+
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
 });
